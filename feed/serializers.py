583f1ba316290f4837a6e5128cdf2d0f3cd16ac9
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-02-04 16:30:10 +0100

    Add PostListSerializer

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from feed.models import Hashtag, Post
+from feed.models import Hashtag, Post, PostImage
 
 
 class HashtagSerializer(serializers.ModelSerializer):
@@ -13,3 +13,34 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ("id", "text", "hashtags")
+
+
+class PostImageListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ("id", "image")
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    num_likes = serializers.SerializerMethodField()
+    num_comments = serializers.SerializerMethodField()
+    images = PostImageListSerializer(many=True, read_only=True)
+
+    @staticmethod
+    def get_num_likes(instance):
+        return instance.likes.count()
+
+    @staticmethod
+    def get_num_comments(instance):
+        return instance.comments.count()
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "text",
+            "hashtags",
+            "num_likes",
+            "num_comments",
+            "images",
+        )
--- feed/views.py
+++ feed/views.py
@@ -2,7 +2,11 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from feed.models import Hashtag, Post
-from feed.serializers import HashtagSerializer, PostSerializer
+from feed.serializers import (
+    HashtagSerializer,
+    PostSerializer,
+    PostListSerializer,
+)
 
 
 class CreateListRetrieveUpdateViewSet(
@@ -16,6 +20,7 @@ class CreateListRetrieveUpdateViewSet(
     A viewset that provides default `create()`, `list()`, `retrieve()`,
     `update()` and `partial_update()` actions.
     """
+
     pass
 
 
@@ -26,7 +31,12 @@ class HashtagViewSet(CreateListRetrieveUpdateViewSet):
 
 class PostViewSet(CreateListRetrieveUpdateViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+
+        return PostSerializer
