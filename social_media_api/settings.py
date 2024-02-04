035ea463e9a9fe8a139254054b41da2c107bb93e
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-02-04 12:07:15 +0100

    Fix codestyle

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$4q@$pz4n1q-thx+x#@)9=ew98%&ynf97#3nt@_q(7btw3l=vt"
+SECRET_KEY = (
+    "django-insecure-$4q@$pz4n1q-thx+x#@)9=ew98%&ynf97#3nt@_q(7btw3l=vt"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
