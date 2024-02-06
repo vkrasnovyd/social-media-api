b10f722bdb1969640c1d37401e0d5a168a0d19c3
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-02-06 16:12:33 +0100

    Add 'followers' and 'followings' endpoints

--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.db.models import Q
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from feed.models import Like
+from user.models import Follow
 from user.serializers import (
     UserInfoSerializer,
     UserInfoListSerializer,
@@ -40,3 +43,33 @@ class UserInfoViewSet(viewsets.ReadOnlyModelViewSet):
 
         context.update({"post_ids_liked_by_user": post_ids_liked_by_user})
         return context
+
+    @action(
+        methods=["GET"], detail=False, url_path=r"(?P<pk>[^/.]+)/followers"
+    )
+    def followers(self, request, pk=None):
+        """Endpoint for getting a list of user's followers."""
+        retrieved_user = self.get_object()
+
+        follow_relations = Follow.objects.filter(following=retrieved_user)
+        followers = get_user_model().objects.filter(
+            followings__in=follow_relations
+        )
+
+        serializer = UserInfoListSerializer(followers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        methods=["GET"], detail=False, url_path=r"(?P<pk>[^/.]+)/followings"
+    )
+    def followings(self, request, pk=None):
+        """Endpoint for getting a list of user's followings."""
+        retrieved_user = self.get_object()
+
+        follow_relations = Follow.objects.filter(follower=retrieved_user)
+        followings = get_user_model().objects.filter(
+            followers__in=follow_relations
+        )
+
+        serializer = UserInfoListSerializer(followings, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
