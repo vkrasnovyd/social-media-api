0106ad673602721659bf3bcf4a13cf342a5733e0
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-02-06 13:25:16 +0100

    Add user search by username, first and last name

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework import viewsets
 
 from user.serializers import (
@@ -8,7 +9,18 @@ from user.serializers import (
 
 
 class UserInfoViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = get_user_model().objects.all()
+    def get_queryset(self):
+        queryset = get_user_model().objects.all()
+
+        search_string = self.request.query_params.get("search", None)
+        if search_string:
+            queryset = queryset.filter(
+                Q(username__icontains=search_string)
+                | Q(first_name__icontains=search_string)
+                | Q(last_name__icontains=search_string)
+            ).distinct()
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
